3816c7d90906ac8898b61df7a9ecd5351172840f
Ghis <piperelghislain@hotmail.com>
2017-03-07 10:15:11 +0100

    rm

--- src/dao/CandidatureHome.java
+++
@@ -1,6 +0,0 @@
-package dao;
-
-public interface CandidatureHome<T> extends Dao<T>{
-	//ajouter les methodes supplémentaires nécessaires à la Dao
-
-}
--- src/model/Candidature.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class Candidature {
-	private int idCandidature;
-
-}
