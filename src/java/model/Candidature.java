118ef86b2f285dec6d5635cfa8c0edbb47bab115
Amilton Cabral Da Veiga <amiltondaveiga@gmail.com>
2017-03-07 16:26:55 +0100

    Classe Candidature faite

--- src/java/model/Candidature.java
+++ src/java/model/Candidature.java
@@ -1,6 +1,40 @@
 package model;
 
 public class Candidature {
-	private int idCandidature;
+    private Personne personne;
+    private SessionFomation sessionFomation;
+    private EtatCandidature etatCandidature;
+
+    public Candidature(Personne personne, SessionFomation sessionFomation, EtatCandidature etatCandidature) {
+        this.personne = personne;
+        this.sessionFomation = sessionFomation;
+        this.etatCandidature = etatCandidature;
+    }
+
+    public Personne getPersonne() {
+        return personne;
+    }
+
+    public void setPersonne(Personne personne) {
+        this.personne = personne;
+    }
+
+    public SessionFomation getSessionFomation() {
+        return sessionFomation;
+    }
+
+    public void setSessionFomation(SessionFomation sessionFomation) {
+        this.sessionFomation = sessionFomation;
+    }
+
+    public EtatCandidature getEtatCandidature() {
+        return etatCandidature;
+    }
+
+    public void setEtatCandidature(EtatCandidature etatCandidature) {
+        this.etatCandidature = etatCandidature;
+    }
+        
+    
 
 }
