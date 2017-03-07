ecb8957c7c88671d79bc72784498f9a5f033be96
Amilton Cabral Da Veiga <amiltondaveiga@gmail.com>
2017-03-07 16:43:47 +0100

    Dictionnaire(HashMap) de Liste de candidature faite

--- src/java/dao/CandidatureHome.java
+++ src/java/dao/CandidatureHome.java
@@ -1,6 +1,10 @@
 package dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public interface CandidatureHome<T> extends Dao<T> {
     //ajouter les methodes supplémentaires nécessaires à la Dao
-
+    public ArrayList<HashMap<String, String>> mapCandidatures()throws SQLException;
 }
