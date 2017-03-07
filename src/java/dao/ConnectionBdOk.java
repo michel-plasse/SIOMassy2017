db86b1ee151642fa2a20177b53ee474336688b8e
Amilton Cabral Da Veiga <amiltondaveiga@gmail.com>
2017-03-07 17:37:16 +0100

    correction des bugs

--- src/java/dao/ConnectionBdOk.java
+++ src/java/dao/ConnectionBdOk.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionBd {
+public class ConnectionBdOk {
 	// private static Connection connection = null;
 
 	public static Connection getConnection() throws SQLException {
@@ -21,7 +21,7 @@ public class ConnectionBd {
 		return connection;
 	}
 
-	private ConnectionBd() {
+	private ConnectionBdOk() {
 
 	}
 }
