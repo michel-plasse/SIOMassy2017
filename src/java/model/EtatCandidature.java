96d3a21367fbd1c01e22877437b6b2564b2593f0
Amilton Cabral Da Veiga <amiltondaveiga@gmail.com>
2017-03-13 11:45:47 +0100

    Creation de la classe EtatCandidature

---
+++ src/java/model/EtatCandidature.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author AveigA
+ */
+class EtatCandidature {
+    private int idEtatCandidature;
+    private String libelle;
+
+    public EtatCandidature(int idEtatCandidature, String libelle) {
+        this.idEtatCandidature = idEtatCandidature;
+        this.libelle = libelle;
+    }
+
+    public int getIdEtatCandidature() {
+        return idEtatCandidature;
+    }
+
+    public void setIdEtatCandidature(int idEtatCandidature) {
+        this.idEtatCandidature = idEtatCandidature;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    
+    
+}
