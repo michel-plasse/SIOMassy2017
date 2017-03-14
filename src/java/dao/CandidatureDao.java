74e43c7edfd03ac98452edd584182390df6bd1a5
Amilton Cabral Da Veiga <amiltondaveiga@gmail.com>
2017-03-14 10:23:36 +0100

    Correction des bugs

--- src/java/dao/CandidatureDao.java
+++ src/java/dao/CandidatureDao.java
@@ -40,59 +40,54 @@ public class CandidatureDao implements CandidatureHome<Candidature> {
         // TODO Auto-generated method stub
         return null;
     }
-    
+
     @Override
-    public ArrayList<HashMap<String, String>> mapCandidatures()throws SQLException{
-            ArrayList<HashMap<String, String>> listeMapCandidature = new ArrayList<HashMap<String, String>>();
-            try {
-                    Connection connection = ConnectionBd.getConnection();
-                    Statement statement = connection.createStatement();
-                    ResultSet resultSet = statement.executeQuery(
-                            "SELECT"
-                                            + " p.nom AS nom,"
-                                        + " p.prenom AS prenom,"
-                                        + " e.libelle AS statut,"
-                                        + " s.date_debut AS debut,"
-                                        + " s.date_fin AS fin,"
-                                        + " f.nom AS formation_nom,"
-                                        + " c.date_effectue AS effectue"
-                            + " FROM"
-                                        + " candidature c"
-                                            + " INNER JOIN"
-                                        + " etat_candidature e ON c.id_etat_candidature = e.id_etat_candidature"
-                                            + " INNER JOIN"
-                                        + " personne p ON c.id_personne = p.id_personne"
-                                            + " INNER JOIN"
-                                        + " session_formation s ON c.id_session = s.id_session"
-                                            + " INNER JOIN"
-                                        + " formation f ON s.id_formation = f.id_formation"
-                            + " ORDER BY date_effectue DESC"
-                            );
-                    while(resultSet.next()){
-                            String nom = resultSet.getString("nom");
-                            String prenom = resultSet.getString("prenom");
-                            String statut = resultSet.getString("statut");
-                            String debut  = resultSet.getString("debut");
-                            String fin  = resultSet.getString("fin");
-                            String formationNom  = resultSet.getString("formation_nom");
-                            String effectue  = resultSet.getString("effectue");
+    public ArrayList<HashMap<String, String>> mapCandidatures() throws SQLException {
+        ArrayList<HashMap<String, String>> listeMapCandidature = new ArrayList<HashMap<String, String>>();
+        Connection connection = ConnectionBdOk.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT"
+                + " p.nom AS nom,"
+                + " p.prenom AS prenom,"
+                + " e.libelle AS statut,"
+                + " s.date_debut AS debut,"
+                + " s.date_fin AS fin,"
+                + " f.nom AS formation_nom,"
+                + " c.date_effectue AS effectue"
+                + " FROM"
+                + " candidature c"
+                + " INNER JOIN"
+                + " etat_candidature e ON c.id_etat_candidature = e.id_etat_candidature"
+                + " INNER JOIN"
+                + " personne p ON c.id_personne = p.id_personne"
+                + " INNER JOIN"
+                + " session_formation s ON c.id_session = s.id_session"
+                + " INNER JOIN"
+                + " formation f ON s.id_formation = f.id_formation"
+                + " ORDER BY date_effectue DESC"
+        );
+        while (resultSet.next()) {
+            String nom = resultSet.getString("nom");
+            String prenom = resultSet.getString("prenom");
+            String statut = resultSet.getString("statut");
+            String debut = resultSet.getString("debut");
+            String fin = resultSet.getString("fin");
+            String formationNom = resultSet.getString("formation_nom");
+            String effectue = resultSet.getString("effectue");
 
-                            HashMap<String, String>	candidature = new HashMap<String, String>();
-                            candidature.put("nom", nom);
-                            candidature.put("prenom", prenom);
-                            candidature.put("statut", statut);
-                            candidature.put("debut", debut);
-                            candidature.put("fin", fin);
-                            candidature.put("formationNom", formationNom);
-                            candidature.put("effectue", effectue);
-                            listeMapCandidature.add(candidature);
+            HashMap<String, String> candidature = new HashMap<String, String>();
+            candidature.put("nom", nom);
+            candidature.put("prenom", prenom);
+            candidature.put("statut", statut);
+            candidature.put("debut", debut);
+            candidature.put("fin", fin);
+            candidature.put("formationNom", formationNom);
+            candidature.put("effectue", effectue);
+            listeMapCandidature.add(candidature);
 
-                    }
-            } catch (SQLException e) {
-
-                    e.printStackTrace();
-            }
-            return listeMapCandidature;
+        }
+        return listeMapCandidature;
     }
 
 }
