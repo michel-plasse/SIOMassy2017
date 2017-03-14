918ce55e5c3a96d72fb5e77ed9560d919e099f4f
Ghis <piperelghislain@hotmail.com>
2017-03-14 13:21:53 +0100

    modif pour Postulerervlet

--- src/java/dao/SessionHome.java
+++ src/java/dao/SessionHome.java
@@ -8,5 +8,6 @@ import model.SessionFormation;
 public interface SessionHome extends Dao<SessionFormation>{
 
 	ArrayList<SessionFormation> getSessionsOuvertes() throws SQLException;
+        public boolean isExistAndOpen(int id) throws SQLException;
 
 }
