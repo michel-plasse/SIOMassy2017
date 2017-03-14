e2dbd2a11134269f7ce253a1189c704b7b999406
nate <iamkapitaan@gmail.com>
2017-03-14 10:58:23 +0100

    Ajout modeldb

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -58,7 +58,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher(request, response);
     }
 
     /**
