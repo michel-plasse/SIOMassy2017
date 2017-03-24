cf7abdb8f6c194d4fdbeb54787a50d8766a7ff2a
admin <admin@PCF306-005.VILGENIS.MASSY>
2017-03-24 10:33:34 +0100

    Save fichier xml.

--- src/java/controller/ListerSessionsOuvertesServlet.java
+++ src/java/controller/ListerSessionsOuvertesServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.ConnectionBd;
 import dao.SessionFormationDao;
 import java.util.logging.Level;
 import java.util.logging.Logger;
