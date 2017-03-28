e47bfa32f6558524d705496dcba5f029874bd2a8
Amilton Cabral Da Veiga <amiltondaveiga@gmail.com>
2017-03-28 12:07:58 +0200

    UI Candidatures

--- src/java/dao/CandidatureDao.java
+++ src/java/dao/CandidatureDao.java
@@ -34,82 +34,31 @@ public class CandidatureDao implements CandidatureHome<Candidature> {
     }
 
     @Override
-    public boolean delete(int id) {
+    public boolean delete(int id) throws SQLException {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public boolean update(int idAncien, Candidature nouveau) {
+    public boolean update(int idAncien, Candidature nouveau) throws SQLException {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public Candidature findById(int id) {
+    public Candidature findById(int id) throws SQLException {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public ArrayList<Candidature> findAll() {
+    public ArrayList<Candidature> findAll() throws SQLException {
         // TODO Auto-generated method stub
         return null;
-    }
+    }    
 
     @Override
-    public ArrayList<HashMap<String, String>> mapCandidatures() throws SQLException {
-        ArrayList<HashMap<String, String>> listeMapCandidature = new ArrayList<HashMap<String, String>>();
-        Connection connection = ConnectionBd.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(
-                "SELECT"
-                + " p.nom AS nom,"
-                + " p.prenom AS prenom,"
-                + " e.libelle AS statut,"
-                + " s.date_debut AS debut,"
-                + " s.date_fin AS fin,"
-                + " f.nom AS formation_nom,"
-                + " c.date_effectue AS effectue"
-                + " FROM"
-                + " candidature c"
-                + " INNER JOIN"
-                + " etat_candidature e ON c.id_etat_candidature = e.id_etat_candidature"
-                + " INNER JOIN"
-                + " personne p ON c.id_personne = p.id_personne"
-                + " INNER JOIN"
-                + " session_formation s ON c.id_session = s.id_session"
-                + " INNER JOIN"
-                + " formation f ON s.id_formation = f.id_formation"
-                + " ORDER BY date_effectue DESC"
-        );
-        
-        while (resultSet.next()) {
-            String nom = resultSet.getString("nom");
-            String prenom = resultSet.getString("prenom");
-            String statut = resultSet.getString("statut");
-            String debut = resultSet.getString("debut");
-            String fin = resultSet.getString("fin");
-            String formationNom = resultSet.getString("formation_nom");
-            String effectue = resultSet.getString("effectue");
-
-            HashMap<String, String> candidature = new HashMap<String, String>();
-            candidature.put("nom", nom);
-            candidature.put("prenom", prenom);
-            candidature.put("statut", statut);
-            candidature.put("debut", debut);
-            candidature.put("fin", fin);
-            candidature.put("formationNom", formationNom);
-            candidature.put("effectue", effectue);
-            listeMapCandidature.add(candidature);
-
-        }
-        
-        return listeMapCandidature;
-    }
-
-    @Override
-    public ArrayList<HashMap<String, String>> findMultiCriteres(ArrayList<String> conditions) throws SQLException {
+    public ArrayList<HashMap<String, String>> mapCandidatures(ArrayList<String> conditions) throws SQLException {
         String where = String.join(" AND ", conditions);
         if (!where.equals("")){
             where = " WHERE " + where;
@@ -140,6 +89,8 @@ public class CandidatureDao implements CandidatureHome<Candidature> {
                 + where
                 + " ORDER BY date_effectue DESC"                
         );
+        
+        System.out.println(where + "heyyyy");
         
         while (resultSet.next()) {
             String nom = resultSet.getString("nom");
---
+++ src/java/dao/EtatCandidatureDao.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.*;;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.EtatCandidature;
+
+/**
+ *
+ * @author admin
+ */
+public class EtatCandidatureDao implements EtatCandidatureHome<EtatCandidature>{
+
+    @Override
+    public void insert(EtatCandidature objetAInserer) throws SQLException {
+    }
+
+    @Override
+    public boolean delete(int id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean update(int idAncien, EtatCandidature nouveau) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public EtatCandidature findById(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<EtatCandidature> findAll() throws SQLException {
+        ArrayList<EtatCandidature> lesEtat = new ArrayList<>();
+        Connection connection = ConnectionBd.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM etat_candidature");
+        
+        while (resultSet.next()) {            
+            int id = resultSet.getInt("id_etat_candidature");
+            String libelle = resultSet.getString("libelle");
+            EtatCandidature etatCandidature = new EtatCandidature(id, libelle);
+            lesEtat.add(etatCandidature);
+        }        
+        return lesEtat;
+    }
+}
