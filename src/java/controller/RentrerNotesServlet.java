0476e78d57a8c5deca031d78664d2c0f43bc09be
admin <admin@PCF306-005.VILGENIS.MASSY>
2017-05-02 12:11:38 +0200

    LE PETIT COMMIT DES FAMILLES LA !

--- src/java/controller/RentrerNotesServlet.java
+++ src/java/controller/RentrerNotesServlet.java
@@ -12,68 +12,81 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import model.Note;
 import model.Personne;
 
 @WebServlet("/rentrerNotes")
 public class RentrerNotesServlet extends HttpServlet {
 
+    public static final String VUE = "/WEB-INF/rentrerNotes.jsp";
+    public static final String VUE_ERROR = "/WEB-INF/message.jsp";
+    
     public RentrerNotesServlet() {
-
+        
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         // verifier que la personne connecté et formateur
         Personne user = (Personne) request.getSession(true).getAttribute("user");
-        if(user == null) {
+        if (user == null) {
             System.out.println("pas connecté");
             // Rediriger vers login
             response.sendRedirect("login");
-        }
-        else if (!user.isEst_formateur()) {
+        } else if (!user.isEst_formateur()) {
             System.out.println("pas formateur");
             request.setAttribute("message", "Vous n'êtes pas un formateur : vous ne pouvez pas créer une évaluation");
             request.getRequestDispatcher("/WEB-INF/message.jsp").forward(request, response);
-        }
-        else {
-
+        } else {
             // verifier que idEvaluation est entier
-            // verifier que c'esr une eval du formateur
-            String vue = "/WEB-INF/rentrerNotes.jsp";
-            EvaluationDao dao = new EvaluationDao();
-
+            String idEvaluationParam = request.getParameter("id");
+            int idEvaluation = Integer.parseInt(idEvaluationParam);
+            // verifier que c'est une eval du formateur
+            NoteDao noteDao = new NoteDao();
+            boolean reponse = false;
+            
             try {
-                ArrayList<Note> lesNotes = dao.findByEval(1);
-                request.setAttribute("lesNotes", lesNotes);
+                reponse = noteDao.checkIdFormateur(idEvaluation, user.getId());
             } catch (SQLException ex) {
                 Logger.getLogger(RentrerNotesServlet.class.getName()).log(Level.SEVERE, null, ex);
-                request.setAttribute("message", "Pb avec la base de données");
-                vue = "/WEB-INF/message.jsp";
             }
-
-            this.getServletContext().getRequestDispatcher(vue).forward(request, response);
-            doPost(request, response);
+            if (reponse) {
+                
+                EvaluationDao dao = new EvaluationDao();
+                
+                try {
+                    ArrayList<Note> lesNotes = dao.findByEval(idEvaluation);
+                    request.setAttribute("lesNotes", lesNotes);
+                } catch (SQLException ex) {
+                    Logger.getLogger(RentrerNotesServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    request.setAttribute("message", "Pb avec la base de données");
+                    this.getServletContext().getRequestDispatcher(VUE_ERROR).forward(request, response);
+                    return;
+                }
+                this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+            } else {
+                response.sendRedirect("espacePersoFormateur");
+            }
+            
         }
     }
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String vue = "/WEB-INF/rentrerNotes.jsp";
         NoteDao dao = new NoteDao();
         Personne etudiant = new Personne();
-
+        
         String[] ids = request.getParameterValues("id");
         String[] notes = request.getParameterValues("note");
         String[] coms = request.getParameterValues("commentaire");
         assert notes.length >= coms.length;
-
+        
         for (int i = 0; i < notes.length; i++) {
             Integer id = Integer.parseInt(ids[i]);
             Double note = (notes[i].equals("")) ? null : Double.parseDouble(notes[i]);
             String commentaire = (coms[i].equals("")) ? "" : coms[i];
-
+            
             Note noteAjoutee = new Note();
             noteAjoutee.setId_note(id);
             noteAjoutee.setNote(note);
@@ -87,12 +100,12 @@ public class RentrerNotesServlet extends HttpServlet {
                 Logger.getLogger(RentrerNotesServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
+        
         String message = "Vos ids : " + String.join(", ", ids)
                 + "<br/>Vos notes : " + String.join(", ", notes)
                 + "<br/>Vos commentaires : " + String.join(", ", coms);
         request.setAttribute("message", message);
         request.getRequestDispatcher("/WEB-INF/message.jsp").forward(request, response);
     }
-
+    
 }
--- src/java/dao/NoteHome.java
+++ src/java/dao/NoteHome.java
@@ -7,5 +7,6 @@ import model.Note;
 public interface NoteHome extends Dao <Note> {
     
     public ArrayList<Note> findByIdEleveNoteEval(int id) throws SQLException;
+    public boolean checkIdFormateur (int idEval, int idFormateur)throws SQLException;
     
 }
